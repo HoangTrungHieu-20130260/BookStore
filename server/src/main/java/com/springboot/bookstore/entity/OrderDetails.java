0bd2fa87a33cabb089dff21416679a6a5976f118
NguyenTrungHieu-20130262 <20130262@st.hcmuaf.edu.vn>
2024-06-01 22:12:34 +0700

    complete user login

--- server/src/main/java/com/springboot/bookstore/entity/OrderDetails.java
+++ server/src/main/java/com/springboot/bookstore/entity/OrderDetails.java
@@ -1,45 +1,46 @@
-package com.springboot.bookstore.entity;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Data
-@Entity
-@Table(name = "order_details")
-public class OrderDetails {
-
-    @Id
-    @GeneratedValue
-    @Column(name = "id")
-    private long id;
-
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id")
-    private Order order;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private int id;
-
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    @JsonBackReference
-    private Category category;
-
-    @Column(name = "title")
-    private String title;
-
-    @Column(name = "current_price")
-    private int currentPrice;
-
-    @Column(name = "quantity", nullable = false)
-    private int quantity;
-
-}
+//package com.springboot.bookstore.entity;
+//
+//import com.fasterxml.jackson.annotation.JsonBackReference;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.ToString;
+//
+//@Data
+//@Entity
+//@Table(name = "order_details")
+//public class OrderDetails {
+//
+//    @Id
+//    @GeneratedValue
+//    @Column(name = "id")
+//    private long id;
+//
+//    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "order_id")
+//    private Order order;
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id", nullable = false)
+//    private int id;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "category_id")
+//    @JsonBackReference
+//    private Category category;
+//
+//    @Column(name = "title")
+//    private String title;
+//
+//    @Column(name = "current_price")
+//    private int currentPrice;
+//
+//    @Column(name = "quantity", nullable = false)
+//    private int quantity;
+//
+//}
